8fff320b2e654bff513a9db691e17888074827ee
Eetu Hirvonen <eetu.hirvonen@edu.karelia.fi>
2024-10-29 15:33:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Frontend ja backend Dockerfile tiedostot löytyvät omista kansioistaan.
+
+Docker kontit:
+
+- docker pull bittrippy/frontend-image:1.0
+- docker pull bittrippy/backend-image:1.0
---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const shoppingCart = [
+  {
+    id: 1,
+    product: "Book",
+    price: 20.5,
+  },
+  {
+    id: 2,
+    product: "Pen",
+    price: 0.25,
+  },
+  {
+    id: 3,
+    product: "Ruler",
+    price: 0.3,
+  },
+  {
+    id: 4,
+    product: "Notebook",
+    price: 9.99,
+  },
+  {
+    id: 5,
+    product: "Eraser",
+    price: 0.5,
+  },
+];
+
+app.get("/list", (req, res) => {
+  res.send(shoppingCart);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
